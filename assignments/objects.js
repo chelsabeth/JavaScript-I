0181fbcf5a2fedb7d616c5215cfea36a68fe6771
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-08-19 15:38:49 -0400

    intern objects done

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,6 +19,40 @@ const example = {
 
 // Write your intern objects here:
 
+const employeeZero = {
+  id: '1',
+  name: 'Mitzi',
+  email: 'mmelloy0@psu.edu',
+  gender: 'F', 
+};
+
+const employeeOne = {
+  id: '2',
+  name: 'Kennan',
+  email: 'kdiben1@tinypic.com',
+  gender: 'M',
+};
+
+const employeeTwo = {
+  id: '3',
+  name: 'Keven',
+  email: 'kmummery2@wikimedia.org,',
+  gender: 'M',
+};
+
+const employeeThree = {
+  id: '4',
+  name: 'Gannie',
+  email: 'gmartinson3@illinois.edu', 
+  gender: 'M',
+};
+
+const employeeFour = {
+  id: '5',
+  name: 'Antonietta',
+  email: 'adaine5@samsung.com',
+  gender: 'F'
+};
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
