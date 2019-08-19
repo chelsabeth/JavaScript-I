b7072e7fd8056f5941e480503b85962871dc0c32
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-08-19 16:28:09 -0400

    done objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -31,6 +31,11 @@ const employeeOne = {
   name: 'Kennan',
   email: 'kdiben1@tinypic.com',
   gender: 'M',
+   
+ // function for speak
+ speak: function(){
+   return  "Hello, my name is Kennan!";
+ }
 };
 
 const employeeTwo = {
@@ -51,29 +56,32 @@ const employeeFour = {
   id: '5',
   name: 'Antonietta',
   email: 'adaine5@samsung.com',
-  gender: 'F'
-};
+  gender: 'F',
 
+  multiply: function(num1,num2) {
+    return num1 * num2;
+  }
+};
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(employeeZero.name);
 // Kennan's ID
-
+console.log(employeeOne.id);
 // Keven's email
-
+console.log(employeeTwo.email);
 // Gannie's name
-
+console.log(employeeThree.name);
 // Antonietta's Gender
-
+console.log(employeeFour.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log(employeeOne.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(employeeFour.multiply(3,4));
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
